9a3629b75eaa23008b57592fbef01ef36a87feb1
jjsimo <s.tufariello@student.vu.nl>
2024-02-07 18:54:43 +0100

    added clean cargo app

---
+++ sha1_hash_cracker/target/debug/deps/sha1_hash_cracker.d
@@ -0,0 +1,5 @@
+C:\Users\simone-jj\Documents\Code\Rust\sha1_hash_cracker\target\debug\deps\sha1_hash_cracker.exe: src\main.rs
+
+C:\Users\simone-jj\Documents\Code\Rust\sha1_hash_cracker\target\debug\deps\sha1_hash_cracker.d: src\main.rs
+
+src\main.rs:
---
+++ sha1_hash_cracker/target/debug/sha1_hash_cracker.d
@@ -0,0 +1,1 @@
+C:\Users\simone-jj\Documents\Code\Rust\sha1_hash_cracker\target\debug\sha1_hash_cracker.exe: C:\Users\simone-jj\Documents\Code\Rust\sha1_hash_cracker\src\main.rs
