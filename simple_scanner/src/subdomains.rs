aea0be95b9654d883c0bc31e9061f86dbeefc591
jjsimo <s.tufariello@student.vu.nl>
2024-02-13 13:20:18 +0100

    implemented subdomains file

--- simple_scanner/src/subdomains.rs
+++ simple_scanner/src/subdomains.rs
@@ -1,10 +1,9 @@
-pub fn enumerate(http_client: &Client, target: &str) -> Result<Vec<Subdomain>,
-↪ Error> {
+pub fn enumerate(http_client: &Client, target: &str) -> Result<Vec<Subdomain>, Error> {
     let entries: Vec<CrtShEntry> = http_client
-        .get(&format!("https://crt.sh/?q=%25.{}&output=json", target))
-        .send()?
-        .json()?;
-        
+        .get(&format!("https://crt.sh/?q=%25.{}&output=json", target))              // HTTP request to crt API (gave subdomain info)
+        .send()?                                                                    // reply is converted in vector of object CrtShEntry
+        .json()?;           // extracts the JSON data from the response and deserializes it into a Rust data type
+
     // clean and dedup results
     let mut subdomains: HashSet<String> = entries
         .into_iter()
