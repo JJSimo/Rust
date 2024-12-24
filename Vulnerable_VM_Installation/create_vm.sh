0f87b07224e1a1c128b5b4273b2a51afbd2c8c70
JJSimo <s.tufariello@student.vu.nl>
2024-12-24 17:22:43 +0100

    worked :)

--- Vulnerable_VM_Installation/create_vm.sh
+++ Vulnerable_VM_Installation/create_vm.sh
@@ -10,15 +10,29 @@ VM_DIR="$HOME/VirtualBox VMs/$VM_NAME"
 # Function to check the previous command
 check_command() {
   if [ $? -ne 0 ]; then
-    echo -e "\e[31m[!] Error: $1\e[0m"
+    print_message "red" "Error: $1"
     exit 1
+  fi
+}
+
+# Function to print messages with color
+print_message() {
+  local color=$1
+  local message=$2
+
+  if [ "$color" == "green" ]; then
+    echo -e "\n\e[32m[*] $message\e[0m"
+  elif [ "$color" == "red" ]; then
+    echo -e "\n\e[31m[!] $message\e[0m"
+  else
+    echo -e "\n$message"
   fi
 }
 
 # Check if VirtualBox is installed
 if ! command -v vboxmanage &> /dev/null; then
-  echo -e "\e[31m[!] VirtualBox is not installed.\e[0m"
-  echo -e "\e[32m[*] Installing VirtualBox...\e[0m"
+  print_message "red" "VirtualBox is not installed."
+  print_message "green" "Installing VirtualBox..."
   
   # Install VirtualBox based on the system package manager
   if [ -f /etc/os-release ]; then
@@ -31,15 +45,15 @@ if ! command -v vboxmanage &> /dev/null; then
       sudo zypper install -y virtualbox
       check_command "VirtualBox installation failed"
     else
-      echo -e "\e[31m[!] Unsupported OS for automatic installation.\e[0m"
+      print_message "red" "Unsupported OS for automatic installation."
       exit 1
     fi
   else
-    echo -e "\e[31m[!] Could not detect the operating system.\e[0m"
+    print_message "red" "Could not detect the operating system."
     exit 1
   fi
 else
-  echo -e "\e[32m[*] VirtualBox is already installed\e[0m"
+  print_message "green" "VirtualBox is already installed"
 fi
 
 # Download Ubuntu ISO
@@ -47,36 +61,40 @@ mkdir -p "$DOWNLOAD_DIR"
 cd "$DOWNLOAD_DIR"
 
 if [ ! -f "$ISO_NAME" ]; then
-  echo -e "\e[32m[*] Downloading Ubuntu 20.04 ISO...\e[0m"
+  print_message "green" "Downloading Ubuntu 20.04 ISO..."
   wget -c "$ISO_URL"
   check_command "Error during ISO downloading"
 else
-  echo -e "\e[32m[*] ISO already downloaded:\e[0m $DOWNLOAD_DIR/$ISO_NAME"
+  print_message "green" "ISO already downloaded: $DOWNLOAD_DIR/$ISO_NAME"
 fi
 
 # Check if the virtual machine already exists
 if vboxmanage list vms | grep -q "\"$VM_NAME\""; then
-  echo -e "\e[32m[*] Virtual machine '$VM_NAME' already exists. Skipping creation.\e[0m"
+  print_message "green" "Virtual machine '$VM_NAME' already exists. Skipping creation."
 else
   # Create the virtual machine in VirtualBox
-  echo -e "\e[32m[*] Creating the virtual machine...\e[0m"
+  print_message "green" "Creating the virtual machine..."
   vboxmanage createvm --name "$VM_NAME" --ostype Ubuntu_64 --register
   check_command "VM creation failed."
 
   # Configure the virtual machine
-  echo -e "\e[32m[*] Setting up the virtual machine...\e[0m"
+  print_message "green" "Setting up the virtual machine..."
   vboxmanage modifyvm "$VM_NAME" \
     --memory 8192 \
     --cpus 6 \
     --nic1 nat \
     --boot1 dvd \
     --vrde on \
-    --graphicscontroller vmsvga
+    --graphicscontroller vmsvga \
+    --clipboard bidirectional \
+    --draganddrop bidirectional \
+    --vram 128
+    
   check_command "VM configuration failed."
 
   # Create a virtual disk
-  echo -e "\e[32m[*] Creating virtual disk (60 GB)\e[0m"
-  vboxmanage createhd --filename "$VM_DIR/$VM_NAME.vdi" --size 61440 
+  print_message "green" "Creating virtual disk (60 GB)"
+  vboxmanage createhd --filename "$VM_DIR/$VM_NAME.vdi" --size 61440
   check_command "Virtual disk creation failed."
 
   # Attach the disk and ISO file
@@ -90,8 +108,8 @@ else
 fi
 
 # Start the VM
-echo -e "\e[32m[*] Starting the virtual machine...\e[0m"
+print_message "green" "Starting the virtual machine..."
 vboxmanage startvm "$VM_NAME" --type gui
 check_command "VM start failed."
 
-echo -e "\e[32m[*] Virtual machine created and started successfully!\e[0m"
+print_message "green" "Virtual machine created and started successfully!"
